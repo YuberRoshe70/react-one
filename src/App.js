49453dd0b127591f999d7c5a077db0bcc70248e7
Yuber <madoss@yandex.ru>
2022-07-16 16:52:05 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -1,61 +1,96 @@
-import React, { createContext } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import axios from "axios";
-//import DB from "./assets/DB.json";
+import DB from "./assets/DB.json";
 import Card from "./components/Card/Card";
 import Header from "./components/Header";
 import InputBlock from "./components/InputBlock";
 import Sidebar from "./components/Sidebar";
 import Skeleton from "./components/Card/Skeleton";
 
-export const Context = React.createContext();
-
 function App() {
-  const [database, setDatabase] = React.useState([]);
+  const [option, setOption] = React.useState(0);
   const [isLoading, setIsLoading] = React.useState(true);
+  const search = useSelector((state) => state.modelReducer.search);
+  //-------------------------------------
+  
+
+  const sortAlfabet = () => {
+    DB.sort((a, b) => a.title.localeCompare(b.title));
+  };
+  const sortRating = () => {
+    return DB.sort((a, b) => a.rating - b.rating).reverse();
+  };
+
+  const sortPrice = () => {
+    return DB.sort((a, b) => a.price - b.price);
+  };
+
+  const sortAll = () => {
+    return DB.sort((a, b) => a.id - b.id);
+  };
+
+  const optionValue = (i) => {
+    if (i === 2) {
+      sortPrice();
+    }
+    if (i === 3) {
+      sortRating();
+    }
+    if (i === 1) {
+      sortAlfabet();
+    }
+    if (i === 0) {
+      sortAll();
+    }
+  };
+
+ 
+  //---------------------------------------
+
+  // React.useEffect(() => {
+  //   axios
+  //     .get("https://run.mocky.io/v3/2eee739b-d763-4962-b1ef-5e12023c698c")
+  //     .then((res) => {
+  //       setDatabase(res.data);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   React.useEffect(() => {
-    axios
-      .get("https://run.mocky.io/v3/2eee739b-d763-4962-b1ef-5e12023c698c")
-      .then((res) => {
-        setDatabase(res.data)
-        setIsLoading(false)
-      })
-  }, [])
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
-  const [search, setSearch] = React.useState("");
   return (
-    <Context.Provider value={{ search, setSearch }}>
-      <div className="App">
-        <div className="container">
-          <Header />
-        </div>
-        <div className="container">
-          <InputBlock />
-        </div>
-        <div className="container">
-          <div className="wrapper">
-            <Sidebar />
-
-            <div className="block">
-              {isLoading
-                ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
-                :  database.filter((item) => {
-                  if (
-                    item.title
-                      .toLocaleLowerCase()
-                      .includes(search.toLocaleLowerCase())
-                  ) {
-                    return true
-                  }
-                  return false
-                }).map((item, i) => <Card {...item} key={i} />)
-                }
-            </div>
+    <div className="App">
+      <div className="container">
+        <Header  />
+      </div>
+      <div className="container">
+        <InputBlock
+          option={option}
+          setOption={setOption}
+          optionValue={(i) => optionValue(i)}
+        />
+      </div>
+      <div className="container">
+        <div className="wrapper">
+          <Sidebar />
+          <div className="block">
+            {isLoading
+              ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
+              : DB.filter((item) =>
+                  item.title
+                    .toLocaleLowerCase()
+                    .includes(search.toLocaleLowerCase())
+                ).map((item, i) => <Card {...item} key={i} />)}
           </div>
         </div>
       </div>
-    </Context.Provider>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setModelsIndex } from '../redux/slices/Modelsslice'
 
 const Header = () => {
- 
+
+  const modelsIndex = useSelector((state) => state.modelReducer.modelsIndex)
+  const dispatch = useDispatch()
+
   const findModel = (i) => {
-    setModelsIndex(i)
+    dispatch(setModelsIndex(i))
   }
 
-  const models = ['ALL AUTO','TOYOTA','MERCEDES','BMW','HONDA', "FORD"]
-  const [modelsIndex, setModelsIndex] = React.useState(0)
+  const models = ['ALL AUTO', 'TOYOTA', 'MERCEDES ', 'BMW', 'HONDA', "FORD"]
+  
 
   return (
     <div className="header">
-    <div className="header__logo">AVTOTRADE</div>
-    <div className="header__menu">
-      <ul className='header__ul'>
-     { models.map((item, i) => <li  onClick={() => findModel(i)} className={ modelsIndex === i ? 'active': ''} key={i}>{item}</li>)  }
-       
-      </ul>
+      <div className="header__logo">AVTOTRADE</div>
+      <div className="header__menu">
+        <ul className='header__ul'>
+          {models.map((item, i) => <li onClick={() => findModel(i)} className={modelsIndex === i ? 'active' : ''} key={i}>{item}</li>)}
+        </ul>
+      </div>
+      <div className="header__cart">Корзина</div>
     </div>
-    <div className="header__cart">Корзина</div>
-  </div>
   )
 }
 
