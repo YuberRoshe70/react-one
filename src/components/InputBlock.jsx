8643f81765f62b1fa38094e1cc587a3763cb5cb4
Yuber <madoss@yandex.ru>
2022-07-16 16:47:00 +0300

    Добавил сортировку

--- src/components/InputBlock.jsx
+++ src/components/InputBlock.jsx
@@ -1,41 +1,42 @@
 
 import React from 'react'
-import { Context } from '../App';
 
-const InputBlock = () => {
 
-  const options = ["алфавиту от А - Я", "цене", "популярности"]
-  const [option, setOption] = React.useState(0)
+import { useSelector, useDispatch } from 'react-redux'
+import { setSearch} from '../redux/slices/Modelsslice.js'
+
+const InputBlock = ({option,setOption, optionValue}) => {
+
+  const search = useSelector((state) => state.modelReducer.search)
+  const dispatch = useDispatch()
+
+  const options = ["Все", "алфавиту от А - Я", "цене", "популярности"]
+  // const [option, setOption] = React.useState(0)
   const [visible, setVisible] = React.useState(false)
-  const { search, setSearch } = React.useContext(Context);
 
   const selected = (i) => {
     setOption(i)
     setVisible(false)
+    optionValue(i)
+   
   }
+
+  
 
 
   return (
     <div className="input__block">
-      <input onChange={(e) => setSearch(e.target.value)} value={search} type="text" placeholder="search..." />
-      {search && <div onClick={() => setSearch('')} className="delete">x</div>}
+      <input onChange={(e) => dispatch( setSearch(e.target.value))} value={search} type="text" placeholder="search..." />
+      {search && <div onClick={() => dispatch(setSearch(''))} className="delete">x</div>}
       <div className="sort" onClick={() => setVisible(!visible)}>
-        <span>{options[option]}</span>
+        <span >{options[option]}</span>
 
       </div>
       {visible && <div className="sort__popup">
         <ul >
           {options.map((item, i) => <li className='popup-value' onClick={() => selected(i)} key={i}>{item}</li>)}
         </ul>
-
       </div>}
-      {/* <select name="" id="" className="block__select"  >
-        <option value=""   >Все</option>
-     { options.map((item, i) =>  <option value={i}  key={i} >{item}</option >) }
-      
-      </select> */}
-
-
     </div>
   )
 }
